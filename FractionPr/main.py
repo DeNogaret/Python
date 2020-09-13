a3b85e353b75cf02dcaf0392e1935a73773a6253
Yaroslav Solokhudinov <yaroslav.solokhudinov@gmail.com>
2020-09-14 00:16:57 +0300

    Tasks

---
+++ FractionPr/main.py
@@ -0,0 +1,7 @@
+import fraction as f
+
+fraction = f.Fraction(7.4, 3.9)
+print("Addition: ", fraction.get_addition())
+print("Subtraction: ", fraction.get_subtraction())
+print("Multiplication: ", fraction.get_multiplication())
+print("Division: ", fraction.get_division())
